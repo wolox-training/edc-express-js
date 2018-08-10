dac522860dc3ccde0ed019179f6c0d2545e18f39
eduardocontreras <eduardo.andre.contreras@gmail.com>
2018-08-09 22:27:48 -0400

    Was removed  property from 'password' attribute and add  property to 'email' attribute.

--- training/migrations/migrations/20180810013634-create-user.js
+++ training/migrations/migrations/20180810013634-create-user.js
@@ -19,11 +19,11 @@ module.exports = {
       },
       email: {
         allowNull: false,
+        unique: true,
         type: Sequelize.STRING
       },
       password: {
         allowNull: false,
-        unique: true,
         type: Sequelize.STRING
       },
       createdAt: {
